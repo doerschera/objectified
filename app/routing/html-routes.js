3e308a83d8ba8097c1996c32d37efc323b5a3e1b
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-04 19:12:02 -0500

    rename home to index

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = function(app) {
 
   app.get('/', function (req, res) {
-		res.sendFile(path.join(__dirname + '/../public/home.html'));
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
 	});
 
   app.get('/quiz', function (req, res) {
